c82f3d0b91a3c4678c3978d15ef1df6665fe746c
Tristan Deloche <tristan.deloche@treatwell.com>
2020-11-08 09:31:28 +0000

    Update reported spec version

--- kmdah-common/src/main/java/moe/tristan/kmdah/common/model/mangadex/MangadexApi.java
+++ kmdah-common/src/main/java/moe/tristan/kmdah/common/model/mangadex/MangadexApi.java
@@ -4,7 +4,7 @@ public final class MangadexApi {
 
     public static final String BASE_URL = "https://api.mangadex.network/";
 
-    public static final int SPEC_VERSION = 16;
+    public static final int SPEC_VERSION = 19;
 
     public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSX";
 
