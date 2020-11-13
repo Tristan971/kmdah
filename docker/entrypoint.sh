75ae6aaa1fff8ccac2c57d59711990a82e2cc4e6
Tristan Deloche <tristan.deloche@treatwell.com>
2020-11-13 19:38:26 +0000

    Revert entrypoint test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 set -x
-exec java -jar "$JARFILE"
+exec java \
+  "-Dspring.config.additional-location=file:$KMDAH_CONFIG_DIR" \
+  "-Dspring.profiles.active=prod" \
+  -jar "$JARFILE"
