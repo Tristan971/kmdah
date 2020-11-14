5420dfdf704f2cd472c09b45a0fb7af7be6913c3
Tristan Deloche <tristan.deloche@treatwell.com>
2020-11-14 19:25:22 +0000

    Sort out S3 clean scanning

--- kmdah-operator/src/main/java/moe/tristan/kmdah/operator/service/vacuum/VacuumService.java
+++ kmdah-operator/src/main/java/moe/tristan/kmdah/operator/service/vacuum/VacuumService.java
@@ -1,5 +1,9 @@
 package moe.tristan.kmdah.operator.service.vacuum;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,13 +11,15 @@ import org.springframework.util.StopWatch;
 import org.springframework.util.unit.DataSize;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import io.micrometer.core.annotation.Timed;
 import moe.tristan.kmdah.common.model.settings.S3Settings;
 import moe.tristan.kmdah.operator.model.CacheSettings;
+import moe.tristan.kmdah.operator.model.vacuum.BucketScanResult;
 
 /**
  * Deletes stuff from the cache, 100-by-100, until we're under the cache size
@@ -38,44 +44,65 @@ public class VacuumService {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        DataSize originalSize = getCacheSize();
-        LOGGER.info("Cache size usage: {}/{} GB", originalSize.toGigabytes(), cacheSettings.getMaxSizeGb());
+        LOGGER.info("Starting bucket scan");
+        DataSize limitSize = DataSize.ofGigabytes(cacheSettings.getMaxSizeGb());
+        BucketScanResult preVacuumScan = scanBucket();
 
-        double vacuumStartFillPercentage = getFillPercentage(originalSize);
-        if (vacuumStartFillPercentage <= 100.0) {
-            LOGGER.info("No need for cache vacuuming ({}% full)", (int) vacuumStartFillPercentage);
+        double originalFill = getFillPercentage(preVacuumScan.getSize(), limitSize);
+        LOGGER.info("Cache size usage: {}% - {}/{} GB", (int) originalFill, preVacuumScan.getSize().toGigabytes(), limitSize.toGigabytes());
+
+        double percentageOfFilesToDelete = originalFill - 100.;
+        int filesToDelete = preVacuumScan.getObjects().size() * (int) percentageOfFilesToDelete;
+
+        if (filesToDelete > 0) {
+            LOGGER.info("Above size threshold, will vacuum {}% of files ({} files)", percentageOfFilesToDelete, filesToDelete);
+            List<S3ObjectSummary> filesDeleted = preVacuumScan
+                .getObjects()
+                .stream()
+                .limit(filesToDelete)
+                .collect(Collectors.toList());
+
+            long deletedSize = filesDeleted.stream().mapToLong(S3ObjectSummary::getSize).sum();
+            List<KeyVersion> deletedKeys = filesDeleted.stream().map(S3ObjectSummary::getKey).map(KeyVersion::new).collect(Collectors.toList());
+
+            DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(s3Settings.getBucketName());
+            deleteObjectsRequest.setKeys(deletedKeys);
+
+            amazonS3.deleteObjects(deleteObjectsRequest);
+            LOGGER.info("Done deleting {} random files - reclaimed {}", deletedKeys, deletedSize);
+        } else {
+            LOGGER.info("Under size threshold, no need to vacuum files");
             return;
         }
 
-        long vacuumed = 0; // vacuum(vacuumStartFillPercentage - 100.0);
-        double vacuumEndFillPercentage = getFillPercentage(getCacheSize());
+        BucketScanResult postVacuumSize = scanBucket();
+        double postVacuumFill = getFillPercentage(postVacuumSize.getSize(), limitSize);
 
         stopWatch.stop();
         LOGGER.info(
-            "Done shrinking cache ({}% -> {}%) - evicted {} chapters in {}s",
-            (int) vacuumStartFillPercentage,
-            (int) vacuumEndFillPercentage,
-            vacuumed,
+            "Done shrinking cache ({}% -> {}%) in {}s",
+            (int) originalFill,
+            (int) postVacuumFill,
             stopWatch.getTotalTimeSeconds()
         );
     }
 
-    private DataSize getCacheSize() {
-        ListObjectsV2Request listObjectsRequest = new ListObjectsV2Request();
-        listObjectsRequest.setBucketName(s3Settings.getBucketName());
-        listObjectsRequest.setMaxKeys(Integer.MAX_VALUE - 1);
-
-        ListObjectsV2Result objectListing = amazonS3.listObjectsV2(listObjectsRequest);
-        LOGGER.info("Bucket contains {} objects", objectListing.getObjectSummaries().size());
-
-        long bucketSize = objectListing.getObjectSummaries().stream().mapToLong(S3ObjectSummary::getSize).sum();
-        return DataSize.ofBytes(bucketSize);
+    private double getFillPercentage(DataSize current, DataSize limit) {
+        return ((double) current.toBytes() / (double) limit.toBytes()) * 100.0;
     }
 
-    private double getFillPercentage(DataSize current) {
-        long maxCacheSizeBytes = DataSize.ofGigabytes(cacheSettings.getMaxSizeGb()).toBytes();
+    private BucketScanResult scanBucket() {
+        ObjectListing objectListing = amazonS3.listObjects(s3Settings.getBucketName());
+        List<S3ObjectSummary> summaries = new ArrayList<>();
+        do {
+            summaries.addAll(objectListing.getObjectSummaries());
+            objectListing = amazonS3.listNextBatchOfObjects(objectListing);
+        } while (objectListing.isTruncated());
 
-        return ((double) current.toBytes() / (double) maxCacheSizeBytes) * 100.0;
+        LOGGER.info("Bucket contains {} objects", summaries.size());
+        long bucketSize = summaries.stream().mapToLong(S3ObjectSummary::getSize).sum();
+
+        return BucketScanResult.of(DataSize.ofBytes(bucketSize), summaries);
     }
 
 }
